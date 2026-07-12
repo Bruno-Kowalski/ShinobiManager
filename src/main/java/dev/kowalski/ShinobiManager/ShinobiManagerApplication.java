d90b2730131637bf91b19f7d8f5bc84711750dc4
kowalski <kowalski@exemple.com>
2026-07-12 13:50:51 -0300

    first commit

---
+++ src/main/java/dev/kowalski/ShinobiManager/ShinobiManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.kowalski.ShinobiManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinobiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinobiManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kowalski/ShinobiManager/ShinobiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kowalski.ShinobiManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinobiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
