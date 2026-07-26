fc698daba85c3ef98a2ea01ae5a02a6e0e1389a1
kowalski <kowalski@exemple.com>
2026-07-26 17:22:58 -0300

    feat: Adiciona o Lombok na classe para abstracao

--- src/main/java/dev/kowalski/ShinobiManager/Ninjas/NinjaModel.java
+++ src/main/java/dev/kowalski/ShinobiManager/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package dev.kowalski.ShinobiManager.Ninjas;
 
 import dev.kowalski.ShinobiManager.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class NinjaModel {
 
@@ -23,36 +27,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreng key ou Chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
